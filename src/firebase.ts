26ab95008ba059582e5fdd659d1c001946816ef1
efrainarape48-ai <efrainarape48@gmail.com>
2026-04-21 16:52:55 -0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,5 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+auth.useDeviceLanguage();
+
 export const db = getFirestore(app);
-export default app;
